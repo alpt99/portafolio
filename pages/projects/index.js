5fd03a0fd18bdab1e7e51ae7703bb570bc477c42
Alejandro Lopez <alpt@uc.com>
2024-03-06 22:19:00 -0300

    starting projects views

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,16 +1,21 @@
 import Link from "next/link";
 import Head from "next/head";
 import Layout from "../../components/layout";
+import ProjectCard from "../../components/projectCard";
 
 export default function ProjectsIndex() {
+
+
     return (
     <Layout>
         <Head>
             <title>Projects</title>
         </Head>
-        <div className="">
-            <h1 className="text-2xl border-black border-2">All the projects are here</h1>
-            <Link href="/">Go back home </Link>
+        <div className="flex flex-col">
+            <ProjectCard title="MVP Web and Mobile App for Transplant Center" description="Spearheaded a 10-member 
+            software engineering team for a MVP, ensuring effective client communication, goal-setting, 
+            and hands-on development of cloud, web, mobile, and backend solutions using AWS, React, React Native, and NodeJS." 
+            src_media="/images/profileAle.jpg" />
         </div>
     </Layout>
     );

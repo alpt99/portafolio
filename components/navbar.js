4cc2be763c8f3df2cc5ea901280e1ee96dac9162
Alejandro Lopez <alpt@uc.com>
2024-03-27 20:37:07 -0300

    arreglos varios de style y daisy

--- components/navbar.js
+++ components/navbar.js
@@ -2,15 +2,37 @@ import Link from "next/link";
 
 export default function Navbar() {
     return(
-        <div className=" px-4 ml-0 mb-2 bg-blue-400 w-full h-14">
-            <nav className="flex w-fit text-white">
-                <div className="mx-4 text-2xl mt-2.5">
-                    <Link href="/">Alejandro López</Link>
-                </div>
-                <div className="mx-4 text-2xl mt-2.5">
-                    <Link href="/projects">Projects</Link>
-                </div>
-            </nav>
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+                <a className="btn btn-ghost text-xl">Alejandro López Torres</a>
+            </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal px-1">
+                <li><Link href="/">About Me</Link></li>
+                <li><Link href="/projects">Projects</Link></li>
+                {/* <li>
+                    <details>
+                    <summary>
+                        Parent
+                    </summary>
+                    <ul className="p-2 bg-base-100 rounded-t-none">
+                        <li><a>Link 1</a></li>
+                        <li><a>Link 2</a></li>
+                    </ul>
+                    </details>
+                </li> */}
+                </ul>
+            </div>
         </div>
+        // <div className=" px-4 ml-0 mb-2 bg-blue-400 w-full h-14">
+        //     <nav className="flex w-fit text-white">
+        //         <div className="mx-4 text-2xl mt-2.5">
+        //             <Link href="/">Alejandro López</Link>
+        //         </div>
+        //         <div className="mx-4 text-2xl mt-2.5">
+        //             <Link href="/projects">Projects</Link>
+        //         </div>
+        //     </nav>
+        // </div>
     );
 }
--- components/projectCard.js
+++ components/projectCard.js
@@ -3,13 +3,13 @@ import Image from 'next/image';
 
 const ProjectCard = ({title, description, src_media, src_media2}) => {
   return (
-    <div className="flex m-16 p-16">
-      <div className="w-1/2">
+    <div className="flex m-10 p-10">
+      <div className="w-1/2 my-20 mx-10">
         {/* Left column */}
-        <h2 className="text-xl font-bold">{title}</h2>
-        <p className="text-gray-500">{description}</p>
+        <h2 className="text-2xl font-bold mb-4">{title}</h2>
+        <p className=" text-justify">{description}</p>
       </div>
-      <div className="w-1/2">
+      <div className="mx-4 divide-x-2">
         {/* Right column */}
         {/* <img src={src_media} alt="Project image or video" className="w-full" /> */}
         <Image src={src_media} alt="Project image or video" width={500} height={300} />

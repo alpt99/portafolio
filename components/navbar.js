069b061f1868eadb329423706cd20712317d77f7
Alejandro Lopez <alpt@uc.com>
2024-03-30 18:47:26 -0300

    arreglo muy chico

--- components/navbar.js
+++ components/navbar.js
@@ -24,15 +24,6 @@ export default function Navbar() {
                 </ul>
             </div>
         </div>
-        // <div className=" px-4 ml-0 mb-2 bg-blue-400 w-full h-14">
-        //     <nav className="flex w-fit text-white">
-        //         <div className="mx-4 text-2xl mt-2.5">
-        //             <Link href="/">Alejandro López</Link>
-        //         </div>
-        //         <div className="mx-4 text-2xl mt-2.5">
-        //             <Link href="/projects">Projects</Link>
-        //         </div>
-        //     </nav>
-        // </div>
+
     );
 }
